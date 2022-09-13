45d131f91b596fa2af82b033b78640439893c32c
aaronare <aaronare@zillowgroup.com>
2022-09-13 09:50:37 -0700

    add in final logs

--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/rolebinding.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/rolebinding.py
@@ -26,6 +26,7 @@ def create_rolebinding(namespace: str, name: str):
         ],
     )
     try:
+        log.info(f'Create: rolebinding {name}')
         rbac_client.create_namespaced_role_binding(namespace, body)
         log.info(f'Successfully created RoleBinding {name}')
     except client.rest.ApiException as e:
@@ -35,6 +36,7 @@ def create_rolebinding(namespace: str, name: str):
 
 def get_rolebinding(namespace: str, name: str) -> client.V1RoleBinding:
     field_selector = f'metadata.name={name}'
+    log.info(f'Get: rolebinding {name}')
     return rbac_client.list_namespaced_role_binding(
         namespace, field_selector=field_selector
     ).items[0]
@@ -42,6 +44,7 @@ def get_rolebinding(namespace: str, name: str) -> client.V1RoleBinding:
 
 def patch_rolebinding(namespace: str, name: str, rb: client.V1RoleBinding):
     try:
+        log.info(f'Patch: rolebinding {name}')
         rbac_client.patch_namespaced_role_binding(name, namespace, rb)
     except client.rest.ApiException as e:
         log.error(f'Exception when calling RbacAuthorizationV1Api->patch_namespaced_role_binding: {e}\n')
@@ -49,6 +52,7 @@ def patch_rolebinding(namespace: str, name: str, rb: client.V1RoleBinding):
 
 def delete_rolebinding(namespace: str, name: str):
     try:
+        log.info(f'Delete: rolebinding {name}')
         rbac_client.delete_namespaced_role_binding(name, namespace)
     except client.rest.ApiException as e:
         log.error(f'Exception when calling RbacAuthorizationV1Api->delete_namespaced_role_binding: {e}\n')
--- components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/serviceaccount.py
+++ components/crud-web-apps/common/backend/kubeflow/kubeflow/crud_backend/api/serviceaccount.py
@@ -19,6 +19,7 @@ def create_serviceaccount(namespace: str, name: str, iam_role: str):
         )
     )
     try:
+        log.info(f'Create: serviceaccount {name}')
         v1_core.create_namespaced_service_account(namespace, body)
         log.info(f'Successfully created ServiceAccount {name}')
     except client.rest.ApiException as e:
@@ -32,6 +33,7 @@ def create_serviceaccount(namespace: str, name: str, iam_role: str):
 def get_serviceaccount(namespace: str, name: str) -> client.V1ServiceAccount:
     authz.ensure_authorized("list", "", "v1", "serviceaccounts", namespace)
     field_selector = f'metadata.name={name}'
+    log.info(f'Get: serviceaccount {name}')
     return v1_core.list_namespaced_service_account(
         namespace, field_selector=field_selector
     ).items[0]
@@ -40,14 +42,16 @@ def get_serviceaccount(namespace: str, name: str) -> client.V1ServiceAccount:
 def patch_serviceaccount(namespace: str, name: str, sa: client.V1ServiceAccount):
     authz.ensure_authorized("patch", "", "v1", "serviceaccounts", namespace)
     try:
+        log.info(f'Patch: serviceaccount {name}')
         v1_core.patch_namespaced_service_account(name, namespace, sa)
     except client.rest.ApiException as e:
-        print(f'Exception when calling CoreV1Api->patch_namespaced_service_account: {e}\n')
+        log.error(f'Exception when calling CoreV1Api->patch_namespaced_service_account: {e}\n')
 
 
 def delete_serviceaccount(namespace: str, name: str):
     authz.ensure_authorized("delete", "", "v1", "serviceaccounts", namespace)
     try:
+        log.info(f'Delete: serviceaccount {name}')
         v1_core.delete_namespaced_service_account(name, namespace)
     except client.rest.ApiException as e:
-        print(f'Exception when calling CoreV1Api->delete_namespaced_service_account: {e}\n')
+        log.error(f'Exception when calling CoreV1Api->delete_namespaced_service_account: {e}\n')
