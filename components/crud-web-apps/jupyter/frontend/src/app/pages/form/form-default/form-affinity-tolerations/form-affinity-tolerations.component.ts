fca6b2095a1bf4c85ad2c820ea6fa342f1cd7767
Snehal Shirgure <snehals@zillowgroup.com>
2022-09-23 17:43:55 +0000

    Changes for affinity/tolerations in KFNB UI

--- components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/form-affinity-tolerations/form-affinity-tolerations.component.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/pages/form/form-default/form-affinity-tolerations/form-affinity-tolerations.component.ts
@@ -12,7 +12,28 @@ export class FormAffinityTolerationsComponent implements OnInit {
   @Input() tolerationGroups: TolerationGroup[];
   @Input() affinityConfigs: AffinityConfig[];
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //on changing node type -- affinityConfig
+    this.parentForm.get('affinityConfig').valueChanges.subscribe(val => {
+      const affinityConfig = this.parentForm.get('affinityConfig').value;
+      // GPU selected
+      if (affinityConfig == 'p2' || affinityConfig == 'p3') {
+        // alert('GPU selected');
+        this.parentForm.get('tolerationGroup').setValue('GPU');
+        this.parentForm.get('gpus').get('num').setValue('');
+        this.parentForm.get('gpus').get('vendor').setValue('nvidia.com/gpu');
+      }
+      // CPU selected
+      else {
+        this.parentForm.get('tolerationGroup').setValue('CPU');
+        this.parentForm.get('gpus').get('num').setValue('');
+        this.parentForm.get('gpus').get('vendor').setValue('');
+        this.parentForm.get('cpu').setValue('0.5');
+        this.parentForm.get('memory').setValue('1.0');
+        this.parentForm.get('storage').setValue('8');
+      }
+    });
+  }
 }
